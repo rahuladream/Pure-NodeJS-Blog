c3fae3ee2374c49d612625d4d5888b001a48f1c6
Singham <rahul.singh4800@gmail.com>
2019-07-31 17:29:32 +0530

    user registration added

--- index.js
+++ index.js
@@ -1,67 +1,41 @@
-const path = require('path')
-const express = require('express')
-const expressEdge = require('express-edge');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Post = require('./database/models/Post');
-const fileUpload = require('express-fileupload');
-
-const app = new express()
-
-app.use(fileUpload());
-app.use(express.static('public'));
-app.use(expressEdge)
-app.set('views',__dirname+'/views');
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-const storePost = require('./middleware/storePost')
-app.use('/posts/store', storePost)
-
-app.get('/', async(req,res) => {
-    const posts = await Post.find({})
-    res.render('index', {
-        posts
-    })
-});
-
-app.get('/posts/new', (req,res) => {
-    res.render('create')
-});
-
-app.get('/post/:id', async(req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    })
-});
-
-app.post('/posts/store', (req, res) => {
-    const {
-        image
-    } = req.files
+const expressEdge = require("express-edge");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
  
-    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-        Post.create({
-            ...req.body,
-            image: `/posts/${image.name}`
-        }, (error, post) => {
-            res.redirect('/');
-        });
-    })
-});
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const createUserController = require('./controllers/createUser')
+const storeUserController = require('./controllers/storeUser');
 
-
+const app = new express();
+ 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
+ 
+app.use(fileUpload());
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set('views', __dirname + '/views');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+const storePost = require('./middleware/storePost')
+ 
+app.use('/posts/store', storePost)
+ 
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", createPostController);
+app.post("/posts/store", storePostController);
+app.get("/auth/register", createUserController);
+app.post('/users/register', storeUserController);
 
-// Starting the app the definite port
 app.listen(4000, () => {
-    console.log('App listening on port 4000')
-})
-
-
-
+  console.log("App listening on port 4000");
+});
