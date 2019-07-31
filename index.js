78e710eb9c5416163fda8e5cb1151ad799a8ae75
Singham <rahul.singh4800@gmail.com>
2019-07-31 16:25:29 +0530

    post new added

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const path = require('path')
 const express = require('express')
+const expressEdge = require('express-edge');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Post = require('./database/models/Post');
+
 const app = new express()
 
+
 app.use(express.static('public'));
+app.use(expressEdge)
+app.set('views',__dirname+'/views');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname,'pages/index.html'))
-})
+    res.render('index')
+});
 
-app.get('/about', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
-})
+app.get('/posts/new', (req,res) => {
+    res.render('create')
+});
 
-app.get('/contact', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
-})
+app.post('/posts/store', (req,res) => {
+    Post.create(req.body, (error,post) => {
+        res.redirect('/')
+    })
+});
 
-app.get('/post', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
-})
+mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+    .then(() => 'You are now connected to Mongo!')
+    .catch(err => console.error('Something went wrong', err))
 
+// Starting the app the definite port
 app.listen(4000, () => {
     console.log('App listening on port 4000')
 })
+
+
+
