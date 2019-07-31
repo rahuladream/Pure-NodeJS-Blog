e6045e4ef34df02d8b49e8f5c6cfd0f315f189c2
Singham <rahul.singh4800@gmail.com>
2019-07-31 17:54:52 +0530

    login and flash message added

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const expressSession = require('express-session');
+const connectMongo = require('connect-mongo');
+const connectFlash = require('connect-flash');
  
 const createPostController = require('./controllers/createPost')
 const homePageController = require('./controllers/homePage')
@@ -10,13 +13,25 @@ const storePostController = require('./controllers/storePost')
 const getPostController = require('./controllers/getPost')
 const createUserController = require('./controllers/createUser')
 const storeUserController = require('./controllers/storeUser');
+const loginController = require('./controllers/login');
+const loginUserController = require('./controllers/loginUser');
 
 const app = new express();
+
+
  
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
- 
+
+const mongoStore = connectMongo(expressSession);
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
+    
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(expressEdge);
@@ -26,13 +41,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
 const storePost = require('./middleware/storePost')
- 
+const auth = require('./middleware/auth');
 app.use('/posts/store', storePost)
+app.use(connectFlash());
  
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
-app.get("/posts/new", createPostController);
+app.get("/posts/new", auth, createPostController);
 app.post("/posts/store", storePostController);
+app.get("/auth/login", loginController);
+app.post("/users/login", loginUserController);
 app.get("/auth/register", createUserController);
 app.post('/users/register', storeUserController);
 
